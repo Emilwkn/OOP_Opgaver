966183cee5e36271b0365ed9b83dbddf31b356d0
Emil Weigelt Kjær Nielsen <emil601b@edu.campusvejle.dk>
2019-11-26 10:29:58 +0100

    Add project files.

---
+++ OOP_Opgaver/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Opgaver
+{
+    class Person
+    {
+        private string name;
+        private int age;
+        private DateTime birhtday;
+
+        public void describePerson()
+        {
+            Console.WriteLine($"{name} is {age} years old.");
+        }
+
+        public Person()
+            : this("no name", 0) { }
+
+        public Person(string name, int age)
+        {
+            name = Name;
+            age = Age;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+    }
+}
---
+++ OOP_Opgaver/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OOP_Opgaver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Mike = new Person()
+            {
+                Name = "Mike",
+                Age = 27
+            };
+
+            Mike.describePerson();
+
+            Console.ReadLine();
+        }
+    }
+}
